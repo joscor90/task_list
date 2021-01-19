e13f2fd27fef34ba8cc05c3b271bdb2c99100275
Josecor <jocorderf@gmail.com>
2021-01-19 10:06:00 -0300

    Defined UI variables in app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// UI Variables
+const form = document.querySelector('#task-form'); 
+const taskList = document.querySelector('ul');
+const clearButton = document.querySelector('#clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('task');
+
+
