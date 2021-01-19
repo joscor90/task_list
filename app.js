404e51ce1b840be087ca75f21066e1e37252877f
Josecor <jocorderf@gmail.com>
2021-01-19 12:32:14 -0300

    Modified storeInLocal function

--- app.js
+++ app.js
@@ -41,9 +41,10 @@ function storeInLocal(task) {
     let tasks;
     if (localStorage.getItem(tasks) === null) {
         tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
     }
     
-    tasks = JSON.parse(localStorage.getItem('tasks'));
     tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
